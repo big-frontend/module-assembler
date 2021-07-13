281fe3f120740c394688220b83e688fcd02ee0dd
JamesfChen <hawks.jamesf@gmail.com>
2021-07-13 22:23:40 +0800

    clean code

--- android/hotel-module/bundle2/src/main/java/com/jamesfchen/bundle2/Bundle2Router.kt
+++ android/hotel-module/bundle2/src/main/java/com/jamesfchen/bundle2/Bundle2Router.kt
@@ -12,7 +12,7 @@ import android.os.Bundle
  * @email: hawksjamesf@gmail.com
  * @since: 六月/30/2021  星期三
  */
-//class Bundle2Router : IRouter {
+//class Bundle2Router : INativeRouter {
 //    override fun call(cxt: Context,bundle:Bundle) {
 //        cxt.startActivity(Intent(cxt,SayHiActivity::class.java))
 //    }
