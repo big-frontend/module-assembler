4d11c4e853d735c495a0710000fd04acad6afff2
JamesfChen <hawks.jamesf@gmail.com>
2022-05-29 16:30:51 +0800

    update

--- composite-builds/bundles-assembler-plugin/src/main/groovy/com/jamesfchen/b/NdBundlePlugin.groovy
+++ composite-builds/bundles-assembler-plugin/src/main/groovy/com/jamesfchen/b/NdBundlePlugin.groovy
@@ -6,7 +6,6 @@ import org.gradle.api.Project
 /**
  * native dynamic bundle
  */
-@Mixin(ReClassPlugin)
 class NdBundlePlugin extends AndroidPlugin {
     def parasite = new ReClassPlugin()
     @Override
