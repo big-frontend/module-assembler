071b8923c0def0efbc7701003e8416cee2d8a5d4
electrolytej <electrolytej@gmail.com>
2025-03-10 15:45:24 +0800

    u

---
+++ module-publisher-gradle-plugin/src/main/java/PublishExtension.java
@@ -0,0 +1,18 @@
+public class PublishExtension {
+    String name;
+    String groupId;
+    String artifactId;
+    String version;
+    String artifactPath;
+    String website;
+    String buildVariant; //只创建指定的变种发布task
+
+    String ossrhUsername;
+    String ossrhPassword;
+    String releasesRepoUrl;
+    String snapshotsRepoUrl;
+
+    String signingKeyId;
+    String signingSecretKeyRingFile;
+    String signingPassword;
+}
